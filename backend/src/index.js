7276c0bed2cd65eb8fd6a973ac963bdc8570c3e3
vagabondraj <raviraj06112005@gmail.com>
2026-03-29 04:35:12 +0530

    Refactor server initialization to use http.createServer and integrate socket.io for real-time capabilities

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,16 +3,21 @@ dotenv.config({
     path: "./.env"
 })
 import app from "./app.js";
+import http from "http";
 import connectToDatabase from "./db/index.js";
 import { DB_NAME } from "./constants.js";
+import { initializeSocket } from "./socket/index.js";
 
 
 connectToDatabase()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    const server = http.createServer(app);
+    initializeSocket(server);
+
+    server.listen(process.env.PORT || 8000, () => {
         console.log(`${DB_NAME} server is running on port ${process.env.PORT}`);
     });
-    app.on("error", (err) => {
+    server.on("error", (err) => {
         console.error("MongoDB connection error:", err);
     });
 })
